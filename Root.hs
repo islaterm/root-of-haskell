c9dcf799920a69913d835f11f1eef369a945ea9a
Ignacio Slater Muñoz <roac@Ignacio-PC.localdomain>
2020-06-25 19:05:28 -0400

    B3: approxLimit implementation

--- Root.hs
+++ Root.hs
@@ -6,7 +6,7 @@
 -- You should have received a copy of the license along with this
 -- work. If not, see <http://creativecommons.org/licenses/by/4.0/>.
 
--- v1.0.0-b.2
+-- v1.0.0-b.3
 module Root where
 
 type Seed = Double
@@ -17,3 +17,12 @@ Creates succesive approximations of r given a seed a0.
 -}
 apps :: Seed -> Double -> Approximations
 apps a0 r = a0 : apps an r where an = 0.5 * (a0 + r / a0)
+
+{-|
+Returns the first element of an infinite list that is at most at a distance e from the previous 
+element
+-}
+approxLimit :: Double -> [Double] -> Double
+approxLimit e xs = if abs ((-) (head xs) (head (tail xs))) <= e
+  then head xs
+  else approxLimit e (tail xs)
