971b8429c2dbe13246195932f03f279b0671db53
SL8R <islaterm@gmail.com>
2020-06-25 10:22:22 -0400

    B1: v1.0.0-b.1 - Added type aliases - apps skeleton

--- Root.hs
+++ Root.hs
@@ -6,6 +6,14 @@
 -- You should have received a copy of the license along with this
 -- work. If not, see <http://creativecommons.org/licenses/by/4.0/>.
 
--- v0.1
+-- v1.0.0-b.1
 module Root where
 
+type Seed = Double
+type Approximations = [Double]
+
+{-|
+Creates succesive approximations of r given a seed a0.
+-}
+apps :: Seed -> Double -> Approximations
+apps a0 r = undefined
